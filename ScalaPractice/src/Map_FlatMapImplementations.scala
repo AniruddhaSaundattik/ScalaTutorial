2223bf2e64c05098ecf1c138571f4b6bc741999c
aniruddha1987-blip <63975956+aniruddha1987-blip@users.noreply.github.com>
2020-05-31 18:22:36 +0530

    Option Map & FlatMap updated

--- ScalaPractice/src/Map_FlatMapImplementations.scala
+++ ScalaPractice/src/Map_FlatMapImplementations.scala
@@ -4,11 +4,18 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 object Map_FlatMapImplementations extends App {
   def myMap[A,B](opt: Option[A])(f: A => B): Option[B] ={
-    Option(f(opt.get))
+   opt match {
+     case Some(value) => Some(f(value))
+     case None => None
+   }
+
   }
 
   def myFlatMap[A,B](opt:Option[A])(f: A => Option[B]): Option[B] = {
-    opt.map(a => f(a)).get
+    opt match {
+      case Some(value) => f(value)
+      case None => None
+   }
   }
 
   def myMapList[A,B](lst: List[A])(f:A => B): List[B] = {
@@ -28,26 +35,29 @@ object Map_FlatMapImplementations extends App {
   }
 
   def myMapFuture[A,B](future: Future[A])(f:A => B): Future[B] = {
-    future.onComplete {
-        case Success(value) => return Future { f(value) }
-        case Failure(exception) => println(exception.getMessage)
-        //case _ => return null
-      }
-    return null
+    future.transform {
+      case Success(value) => Success(f(value))
+      case Failure(exception) => Failure(exception)
+    }
   }
 
   def myFlatMapFuture[A,B](future: Future[A])(f:A => Future[B]): Future[B] = {
-    future.onComplete{
-      case Success(value) => return f(value)
-      case Failure(exception) => println(exception.getMessage)
+    future.transformWith{
+      case Success(value) => f(value)
+      case Failure(exception) => throw exception
     }
-    return null
   }
 
   def myMapEither[A,B,B1](either: Either[A,B])(f:B => B1): Either[A,B1] = {
     either match {
       case Right(value) => Right(f(value))
       case Left(value) => Left(value)
+    }
+  }
+
+  def myFlatMapEither[A,B,B1](either: Either[A,B])(f:B => Either[A,B1]): Either[A,B1] = {
+    either match {
+      case Right(value) => f(value)
     }
   }
 }
